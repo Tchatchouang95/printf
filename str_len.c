5fd75576a270ab3a8b60f85a90ab7b3811d8ca2f
Bethlehem-Hailegebrel <kiyakeys7@gmail.com>
2023-12-04 05:21:30 -0800

    str_len.c

---
+++ str_len.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * str_len - find the length of the string s 
+ * @s: string
+ * Return: length of string
+ */
+int str_len(char *s)
+{
+	int i, count = 0;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
